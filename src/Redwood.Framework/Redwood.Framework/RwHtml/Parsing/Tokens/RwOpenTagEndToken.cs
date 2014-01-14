838e397cecc7707e114e8ef7c510912ff5699e05
Tomas Jecha <tomas@jecha.net>
2014-01-14 11:35:05 +0100

    - added missing file

---
+++ src/Redwood.Framework/Redwood.Framework/RwHtml/Parsing/Tokens/RwOpenTagEndToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Redwood.Framework.RwHtml.Parsing.Tokens
+{
+    public class RwOpenTagEndToken : RwHtmlToken
+    {
+        public bool IsSelfClosing { get; private set; }
+
+        public RwOpenTagEndToken(string tagName, bool isSelfClosing)
+        {
+            IsSelfClosing = isSelfClosing;
+        }
+    }
+}
