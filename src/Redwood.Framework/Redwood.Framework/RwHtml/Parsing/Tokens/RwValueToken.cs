7c7464cd2830d2e1d8541b9c03241ab47c56fa6a
Tomas Jecha <jecha@avast.com>
2014-01-15 20:26:43 +0100

    - added missing files

---
+++ src/Redwood.Framework/Redwood.Framework/RwHtml/Parsing/Tokens/RwValueToken.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Redwood.Framework.Generation;
+
+namespace Redwood.Framework.RwHtml.Parsing.Tokens
+{
+    public class RwValueToken : RwHtmlToken
+    {
+        public RwValueToken(string text, bool isExpression)
+        {
+            Text = text;
+            IsExpression = isExpression;
+        }
+
+        public string Text { get; set; }
+        public bool IsExpression { get; set; }
+
+        public string Expression
+        {
+            get
+            {
+                if (!IsExpression)
+                    throw new InvalidOperationException("This property is not available for instances with IsExpression = false.");
+                return Text;
+            }
+        }
+    }
+}
